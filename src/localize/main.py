ef53f940a9b4fbde2dbec8f089e85a1572f1cb1e
M. Enes Kaya <enes@T420>
2024-04-29 12:27:59 +0200

    Update.

--- src/localize/main.py
+++ src/localize/main.py
@@ -9,24 +9,23 @@ import numpy as np
 
 def main() -> int:
     # 5 cm
-    _, audio = wavfile.read('../../recordings/Recording-7-real-5cm.wav')
+    recording_name = '../../recordings/Recording-7-real-5cm.wav'
+    _, audio = wavfile.read(recording_name)
     y = (defsignal(audio[:, 0]), defsignal(audio[:, 1]))
-    print(f"INFO: distance: {y[0].calculate_distance(y[1])} meter")
+    print(f'INFO: recording({recording_name}): distance: {y[0].calculate_distance(y[1])} meter')
 
     # 1 m
-    _, audio = wavfile.read('../../recordings/Recording-7-real-1m.wav')
+    recording_name = '../../recordings/Recording-7-real-1m.wav'
+    _, audio = wavfile.read(recording_name)
     y = (defsignal(audio[:, 0]), defsignal(audio[:, 1]))
-    print(f'INFO: distance: {y[0].calculate_distance(y[1])} meter')
-
-    # x = 64 cm, y = 40 cm
-    _, audio = wavfile.read('../../recordings/record_x64_y40.wav')
-    y = (defsignal(audio[:, 1]), defsignal(audio[:, 4]))
-    print(f'INFO: distance: {y[0].calculate_distance(y[1])} meter')
+    print(f'INFO: recording({recording_name}): distance: {y[0].calculate_distance(y[1])} meter')
 
     # load all recordings
     recordings = []
     for path, _, files in os.walk('../../recordings'):
         for recording_name in files:
+            if 'real' in recording_name:
+                continue
             recording_name: str = os.path.join(path, recording_name)
             recordings.append(recording_name)
             print(f'INFO: loaded {recording_name}')
@@ -38,9 +37,7 @@ def main() -> int:
         for (idx, ref) in enumerate(reference_channels.T):
             for (jdx, rec) in enumerate(recording_channels.T):
                 distance = defsignal(ref).calculate_distance(rec)
-                print(f'INFO: recording({recording_name}): ', end='')
-                print(f'reference({idx}): channel({jdx}): ', end='')
-                print(f'distance: {distance} meter')
+                print(f'INFO: recording({recording_name}): reference({idx}): channel({jdx}): distance: {distance} m')
 
     return 0
 
